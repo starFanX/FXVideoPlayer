0e9689cb20fa6a272415351ab9540858be459a3d
繁星 <fanxing@fanxingeMacBook.lan>
2017-06-23 22:34:10 +0800

    第一次提交^^

---
+++ FXVideoPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FXVideoPlayer
+//
+//  Created by 繁星 on 17/6/23.
+//  Copyright © 2017年 繁星. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
